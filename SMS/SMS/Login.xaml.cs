8c5721fc02113528bf4340a905114160fc07af8a
STUDENTS\1798064 <mj_hadi@yahoo.com>
2017-10-03 12:30:42 -0400

    no message

---
+++ SMS/SMS/Login.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SMS
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ipd\Documents\SMSDB.mdf;Integrated Security=True;Connect Timeout=30");
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+                string query = "SELECT COUNT(1) FROM USER WHERE Username=@Username and Password=@Password";
+                SqlCommand sqlcmd = new SqlCommand(query, conn);
+                sqlcmd.CommandType = CommandType.Text;
+                sqlcmd.Parameters.AddWithValue("@Username", tbUsername.Text);
+                sqlcmd.Parameters.AddWithValue("@Password", tbPassword.Password);
+                int count = Convert.ToInt32(sqlcmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    MainWindow dashboard = new MainWindow();
+                    dashboard.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username or Password is incorrect.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
